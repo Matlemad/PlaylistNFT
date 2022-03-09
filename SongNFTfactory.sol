6643b73eb2530220a3ffbc0a93e377eac39030f3
Matlemad <35381854+Matlemad@users.noreply.github.com>
2022-03-09 14:59:44 +0100

    Update SongNFTFactory.sol

--- SongNFTfactory.sol
+++ SongNFTfactory.sol
@@ -18,7 +18,7 @@ contract SongNFTfactory is ERC721, ERC721URIStorage, ERC721Burnable, Ownable {
         return "link";
     }
 
-    function safeMint(address to, string memory uri) public onlyOwner {
+    function safeMint(address to, string memory mp3Uri) public onlyOwner {
         uint256 tokenId = _tokenIdCounter.current();
         _tokenIdCounter.increment();
         _safeMint(to, tokenId);
